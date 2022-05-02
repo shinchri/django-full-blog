1509f1c9c303c4eac01d9b049a94a72d50b1ad33
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-05-02 11:42:03 -0500

    feat: Create class-based Home List View

--- theblog/views.py
+++ theblog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from theblog.models import Post
 
 # Create your views here.
-def home(request):
-  return render(request, 'home.html', {})
+class HomeView(ListView):
+  model = Post
+  template_name = "home.html"
