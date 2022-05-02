0c7c5ffd23de3df0c62f4c688c5ad69bd0f2ab1a
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-05-02 12:09:19 -0500

    feat: Add Post Detail page

--- theblog/views.py
+++ theblog/views.py
@@ -7,3 +7,7 @@ from theblog.models import Post
 class HomeView(ListView):
   model = Post
   template_name = "home.html"
+
+class ArticleDetailView(DetailView):
+  model = Post
+  template_name = "article_details.html"
