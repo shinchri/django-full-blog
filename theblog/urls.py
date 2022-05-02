9e31d81bbae738ff57e4cd48368f421cc63c69e6
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-05-02 18:57:10 -0500

    feat: Add feature to update post and edit post form

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AddPostView, HomeView, ArticleDetailView
+from .views import AddPostView, HomeView, ArticleDetailView, UpdatePostView
 
 urlpatterns = [
   path('', HomeView.as_view(), name="home"),
   path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
   path('add_post/', AddPostView.as_view(), name="add_post"),
+  path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
 ]
--- theblog/views.py
+++ theblog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from theblog.models import Post
-from .forms import PostForm
+from .forms import PostForm, EditPostForm
 
 # Create your views here.
 class HomeView(ListView):
@@ -19,3 +19,9 @@ class AddPostView(CreateView):
   template_name = "add_post.html"
   # fields = '__all__' # put all fields
   # fields = ('title', 'body')
+
+class UpdatePostView(UpdateView):
+  model = Post
+  template_name = "update_post.html"
+  form_class = EditPostForm
+  # fields = ['title', 'title_tag', 'body']
