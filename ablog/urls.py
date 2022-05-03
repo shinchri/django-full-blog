05daabafcdf90cbadf78b0cc1499dc9aa8735a9e
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-05-03 10:23:44 -0500

    feat: Allow authenticated user to see edit/update links and finish authentication

--- ablog/urls.py
+++ ablog/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('theblog.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls'))
 ]
+
+
+# from django.contrib.auth import views
+# path("login/", views.LoginView.as_view(), name="login"),
+# path("logout/", views.LogoutView.as_view(), name="logout"),
