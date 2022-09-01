b9b495776cdad4e7b5157f4d130c9ab5779ef1ff
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-09-01 07:50:16 -0500

    feat: Add article snippets

--- theblog/forms.py
+++ theblog/forms.py
@@ -13,7 +13,7 @@ choices = Category.objects.all().values_list('name', 'name')
 class PostForm(forms.ModelForm):
   class Meta:
     model = Post
-    fields = ('title', 'title_tag', 'author', 'category', 'body')
+    fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet')
 
     widgets = {
       'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title...'}),
@@ -22,16 +22,18 @@ class PostForm(forms.ModelForm):
       #'author': forms.Select(attrs={'class': 'form-control'}),
       'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
       'body': forms.Textarea(attrs={'class': 'form-control'}),
+      'snippet': forms.Textarea(attrs={'class': 'form-control'}),
     }
 
 class EditPostForm(forms.ModelForm):
   class Meta:
     model = Post
-    fields = ('title', 'title_tag', 'category', 'body')
+    fields = ('title', 'title_tag', 'category', 'body', 'snippet')
 
     widgets = {
       'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title...'}),
       'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
       'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
       'body': forms.Textarea(attrs={'class': 'form-control'}),
+      'snippet': forms.Textarea(attrs={'class': 'form-control'}),
     }
